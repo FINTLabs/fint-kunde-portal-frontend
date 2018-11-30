6cdd1dfa62557a2332dd30a8d5bb676dfa204972
Frode Sjovatsen <frode@fintprosjektet.no>
2018-11-30 21:29:41 +0100

    wip for issue #10

--- src/features/link-walker/CvsReport.js
+++ src/features/link-walker/CvsReport.js
@@ -1,26 +1,57 @@
+import XLSX from 'xlsx';
+
 class CvsReport {
 
-  static getReport(test) {
+    static getReport(test) {
 
-    const relations = test.relations;
-    let report = [];
-    Object.entries(relations).forEach(([k, v]) => {
-      let row;
-      v.forEach(test => {
-        if (test !== undefined) {
-          row = {
-            relation: k,
-            status: test.status,
-            reason: test.reason,
-            url: test.url,
-          }
-          report.push(row);
-        }
-      });
 
-    });
-    return report;
-  }
+        const relations = test.relations;
+        let report = [];
+        Object.entries(relations).forEach(([k, v]) => {
+            let row;
+            v.forEach(test => {
+                if (test !== undefined) {
+                    row = {
+                        RELATION: k,
+                        STATUS: test.status,
+                        REASON: test.reason,
+                        URL: {v: test.url.replace('felleskomponent.no/', 'felleskomponent.no/?/'),
+                            l: {
+                                Target: test.url.replace('felleskomponent.no/', 'felleskomponent.no/?/'),
+                                Tooltip: "Klikk for å gå til test klienten"
+                            }
+                        },
+                    };
+                    report.push(row);
+                }
+            });
+
+        });
+
+        return report;
+    }
+
+    static getExcelReport(test) {
+        let wb = XLSX.utils.book_new();
+
+
+        let ws = XLSX.utils.json_to_sheet(CvsReport.getReport(test));
+        ws['!autofilter'] = {ref: "A1:D1"};
+        ws['!cols'] = [{wch: 25}, {wch: 10}, {wch: 25}, {wch: 100}];
+        //ws['!rows'] = [{hpx: 20}];
+
+        XLSX.utils.book_append_sheet(wb, ws, "Test rapport");
+
+
+        const wbout = XLSX.write(wb, {bookType: 'xlsx', type: 'binary'});
+
+        let buf = new ArrayBuffer(wbout.length); //convert s to arrayBuffer
+        let view = new Uint8Array(buf);  //create uint8array as viewer
+        for (let i = 0; i < wbout.length; i++) view[i] = wbout.charCodeAt(i) & 0xFF; //convert to octet
+        return buf;
+
+
+    }
 }
 
 export default CvsReport;
