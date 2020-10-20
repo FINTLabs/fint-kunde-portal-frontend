4694a9d56afd72ff686a1d994d9917e4ee1d13db
OlaStalberg <olastaalberg@gmail.com>
2020-10-20 13:45:37 +0200

    FKP-4 solved, imports optimized

--- src/data/redux/dispatchers/contact.js
+++ src/data/redux/dispatchers/contact.js
@@ -1,41 +1,35 @@
-import {
-  createContactSuccess,
-  deleteContactSuccess,
-  fetchContactError,
-  fetchContactsSuccess,
-  updateKontaktSuccess
-} from "../actions/contacts";
+import {deleteContactSuccess, fetchContactError, fetchContactsSuccess, updateKontaktSuccess} from "../actions/contacts";
 import ContactApi from "../../api/ContactApi";
 
 export function fetchContacts() {
-  return (dispatch) => {
-    return ContactApi.fetchContacts().then(([response, json]) => {
-      if (response.status === 200) {
-        dispatch(fetchContactsSuccess(json));
-      } else {
-        dispatch(fetchContactError());
-      }
-    })
-  }
+    return (dispatch) => {
+        return ContactApi.fetchContacts().then(([response, json]) => {
+            if (response.status === 200) {
+                dispatch(fetchContactsSuccess(json));
+            } else {
+                dispatch(fetchContactError());
+            }
+        })
+    }
 }
 
 export function updateContact(kontakt) {
-  return function (dispatch) {
-    return ContactApi.updateContact(kontakt).then(response => {
-      dispatch(updateKontaktSuccess(response));
-      return response;
-    }).catch(error => {
-      throw (error);
-    });
-  };
+    return function (dispatch) {
+        return ContactApi.updateContact(kontakt).then(response => {
+            dispatch(updateKontaktSuccess(response));
+            return response;
+        }).catch(error => {
+            throw (error);
+        });
+    };
 }
 
 export function deleteContact(kontakt) {
-  return function (dispatch) {
-    return ContactApi.deleteContact(kontakt).then(() => {
-      dispatch(deleteContactSuccess(kontakt));
-    }).catch(error => {
-      throw (error);
-    })
-  }
+    return function (dispatch) {
+        return ContactApi.deleteContact(kontakt).then(() => {
+            dispatch(deleteContactSuccess(kontakt));
+        }).catch(error => {
+            throw (error);
+        })
+    }
 }
