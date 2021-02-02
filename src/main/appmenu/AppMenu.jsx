682cf35c650fc2d6ec380cdc3b88786f782a780b
Frode Sjovatsen <frode@fintlabs.no>
2021-02-02 12:58:09 +0100

    Added fullname to appbar

--- src/main/appmenu/AppMenu.jsx
+++ src/main/appmenu/AppMenu.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
+import {withStyles} from "@material-ui/core/styles";
 import classNames from "classnames";
 import Drawer from "@material-ui/core/Drawer";
 import AppBar from "@material-ui/core/AppBar";
@@ -12,180 +12,193 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
-import { MENU_ITEMS } from "./MenuItems";
+import {MENU_ITEMS} from "./MenuItems";
 import Routes from "../routes/Routes";
 import OrganisationSelector from "./OrganisationSelector";
 
 const drawerWidth = 240;
 
 const styles = theme => ({
-  root: {
-    flexGrow: 1,
-    height: "100%",
-    zIndex: 1,
-    overflow: "hidden",
-    position: "relative",
-    display: "flex"
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  appFrame: {
-    position: "relative",
-    display: "flex",
-    width: "100%",
-    height: "100%"
-  },
-  menuButton: {
-    marginLeft: 12,
-    marginRight: 36
-  },
-  hide: {
-    display: "none"
-  },
-  drawerPaper: {
-    position: "relative",
-    whiteSpace: "nowrap",
-    width: drawerWidth,
-    transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  drawerPaperClose: {
-    overflowX: "hidden",
-    transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    width: theme.spacing(7),
-    [theme.breakpoints.up("sm")]: {
-      width: theme.spacing(9)
-    }
-  },
-  toolbar: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    padding: "0 8px",
-    ...theme.mixins.toolbar
-  },
-  content: {
-    width: "100%",
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: 24,
-    height: "calc(100% - 56px)",
-    marginTop: 56,
-    [theme.breakpoints.up("sm")]: {
-      height: "calc(100% - 64px)",
-      marginTop: 64
-    }
-  },
-  logo: {
-    height: "8%",
-    width: "8%",
-    marginRight: theme.spacing(4)
-  },
-  flex: {
-    flex: 1
-  }
+    root: {
+        flexGrow: 1,
+        height: "100%",
+        zIndex: 1,
+        overflow: "hidden",
+        position: "relative",
+        display: "flex"
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(["width", "margin"], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen
+        })
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(["width", "margin"], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen
+        })
+    },
+    appFrame: {
+        position: "relative",
+        display: "flex",
+        width: "100%",
+        height: "100%"
+    },
+    menuButton: {
+        marginLeft: 12,
+        marginRight: 36
+    },
+    hide: {
+        display: "none"
+    },
+    drawerPaper: {
+        position: "relative",
+        whiteSpace: "nowrap",
+        width: drawerWidth,
+        transition: theme.transitions.create("width", {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen
+        })
+    },
+    drawerPaperClose: {
+        overflowX: "hidden",
+        transition: theme.transitions.create("width", {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen
+        }),
+        width: theme.spacing(7),
+        [theme.breakpoints.up("sm")]: {
+            width: theme.spacing(9)
+        }
+    },
+    toolbar: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-end",
+        padding: "0 8px",
+        ...theme.mixins.toolbar
+    },
+    content: {
+        width: "100%",
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.default,
+        padding: 24,
+        height: "calc(100% - 56px)",
+        marginTop: 56,
+        [theme.breakpoints.up("sm")]: {
+            height: "calc(100% - 64px)",
+            marginTop: 64
+        }
+    },
+    logo: {
+        height: "8%",
+        width: "8%",
+        marginRight: theme.spacing(4)
+    },
+    flex: {
+        flex: 1
+    },
+    flexName: {
+        flex: 1,
+        textAlign: "end",
+        marginBottom: "2px"
+    },
 });
 
 class AppMenu extends React.Component {
-  state = {
-    open: false
-  };
+    state = {
+        open: false
+    };
 
-  handleDrawerOpen = () => {
-    this.setState({ open: true });
-  };
+    handleDrawerOpen = () => {
+        this.setState({open: true});
+    };
 
-  handleDrawerClose = () => {
-    this.setState({ open: false });
-  };
+    handleDrawerClose = () => {
+        this.setState({open: false});
+    };
 
-  render() {
-    const { classes, theme } = this.props;
+    render() {
+        const {classes, theme, me} = this.props;
 
-    return (
-      <div className={classes.appFrame}>
-        <AppBar
-          position="absolute"
-          className={classNames(
-            classes.appBar,
-            this.state.open && classes.appBarShift
-          )}
-        >
-          <Toolbar disableGutters={!this.state.open}>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              onClick={this.handleDrawerOpen}
-              className={classNames(
-                classes.menuButton,
-                this.state.open && classes.hide
-              )}
-            >
-              <MenuIcon />
-            </IconButton>
-            <img src="/fint.svg" alt="logo" className={classes.logo} />
-            <Typography
-              variant="h6"
-              color="inherit"
-              noWrap
-              className={classes.flex}
-            >
-              Kundeportal
-            </Typography>
-            <OrganisationSelector />
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          variant="permanent"
-          classes={{
-            paper: classNames(
-              classes.drawerPaper,
-              !this.state.open && classes.drawerPaperClose
-            )
-          }}
-          open={this.state.open}
-        >
-          <div className={classes.toolbar}>
-            <IconButton onClick={this.handleDrawerClose}>
-              {theme.direction === "rtl" ? (
-                <ChevronRightIcon />
-              ) : (
-                <ChevronLeftIcon />
-              )}
-            </IconButton>
-          </div>
-          <Divider />
-          <List>{MENU_ITEMS}</List>
-        </Drawer>
-        <main className={classes.content}>
-          <Routes />
-        </main>
-      </div>
-    );
-  }
+        return (
+            <div className={classes.appFrame}>
+                <AppBar
+                    position="absolute"
+                    className={classNames(
+                        classes.appBar,
+                        this.state.open && classes.appBarShift
+                    )}
+                >
+                    <Toolbar disableGutters={!this.state.open}>
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            onClick={this.handleDrawerOpen}
+                            className={classNames(
+                                classes.menuButton,
+                                this.state.open && classes.hide
+                            )}
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+                        <img src="/fint.svg" alt="logo" className={classes.logo}/>
+                        <Typography
+                            variant="h6"
+                            color="inherit"
+                            noWrap
+                            className={classes.flex}
+                        >
+                            Kundeportal
+                        </Typography>
+                        <Typography
+                            variant={"subtitle1"}
+                            color="inherit"
+                            noWrap
+                            className={classes.flexName}
+                        >
+                            {me.firstName + " " + me.lastName + "  |"}
+                        </Typography>
+                        <OrganisationSelector/>
+                    </Toolbar>
+                </AppBar>
+                <Drawer
+                    variant="permanent"
+                    classes={{
+                        paper: classNames(
+                            classes.drawerPaper,
+                            !this.state.open && classes.drawerPaperClose
+                        )
+                    }}
+                    open={this.state.open}
+                >
+                    <div className={classes.toolbar}>
+                        <IconButton onClick={this.handleDrawerClose}>
+                            {theme.direction === "rtl" ? (
+                                <ChevronRightIcon/>
+                            ) : (
+                                <ChevronLeftIcon/>
+                            )}
+                        </IconButton>
+                    </div>
+                    <Divider/>
+                    <List>{MENU_ITEMS}</List>
+                </Drawer>
+                <main className={classes.content}>
+                    <Routes/>
+                </main>
+            </div>
+        );
+    }
 }
 
 AppMenu.propTypes = {
-  classes: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired
+    classes: PropTypes.object.isRequired,
+    theme: PropTypes.object.isRequired
 };
 
-export default withStyles(styles, { withTheme: true })(AppMenu);
+export default withStyles(styles, {withTheme: true})(AppMenu);
