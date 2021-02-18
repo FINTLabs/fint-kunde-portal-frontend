befa35759a3db494f4599d1218ce4e3bea791eb8
OlaStalberg <olastaalberg@gmail.com>
2021-02-18 09:54:54 +0100

    added some tests

---
+++ cypress/integration/adapters/tests.js
@@ -0,0 +1,106 @@
+describe('Testing adapters page', () => {
+    it('Clicking on menu item for adapter brings you to correct page', () => {
+        cy.apiIntercept();
+        cy.goToHome();
+        cy.get("#menuBurger").click();
+        cy.get("#menuList").children().contains("Adapter").click();
+        cy.url().should('include', '/adapters');
+    });
+    it('Information about adapter should be available', () => {
+            cy.get("p").first().should('be.visible');
+            cy.get("p").first().contains("Ett adapter er påloggingsinformasjon");
+        }
+    );
+    it('Adapter should show as expected', () => {
+            cy.get("#adapterList").should('be.visible');
+        }
+    );
+    it('Components list should be 5 lines', () => {
+            cy.get("#adapterList")
+                .find('li')
+                .should(
+                    ($tr) => {
+                        expect($tr).to.have.length(2)
+                    }
+                )
+        }
+    );
+    it('Clicking edit adapter button opens dialog', () => {
+        cy.get("#adapterList").find("button").first().click();
+        cy.get('#warning-dialog-content').should('be.visible');
+    });
+    /*
+    it('Confirming removing component', () => {
+        cy.removeComponentApiCall();
+        cy.get("#confirm").click();
+    });
+    it('Alert field is visible after remove click', () => {
+        cy.get("#notifySnackbar").should("be.visible");
+    });
+    it('Remove Icon should change to add icon', () => {
+        cy
+            .get("#componentList")
+            .find("button")
+            .first()
+            .invoke('attr', 'aria-label')
+            .should('contain', 'Add');
+    });
+    it('Clicking settings should give a pop up with component settings', () => {
+        cy.get("#componentList").find("button").first().next().click();
+        cy.get("#form-dialog-title").should('be.visible');
+    });
+    it('Title should include component name', () => {
+        cy.get("#form-dialog-title").contains("Test Kodeverk");
+    });
+    it('Name should be visible and contain correct value', () => {
+        cy.get("#componentNameCell").should("be.visible");
+        cy.get("#componentNameCell").contains("Test kodeverk");
+    });
+    it('Description should be visible and contain correct value', () => {
+        cy.get("#componentDescriptionCell").should("be.visible");
+        cy.get("#componentDescriptionCell").contains("Test Kodeverk");
+    });
+    it('Base Path should contain correct value', () => {
+        cy.get("#componentBasePathCell").should("be.visible");
+        cy.get("#componentBasePathCell").contains("/test/kodeverk");
+    });
+    it('Open Data should have correct check value', () => {
+        cy.get("#componentOpenDataCheckbox").should("not.be.checked");
+    });
+    it('Common should have correct check value', () => {
+        cy.get("#componentCommonCheckbox").should("not.be.checked");
+    });
+    it('Production should have correct check value', () => {
+        cy.get("#componentProductionCheckbox").should("not.be.checked");
+    });
+    it('In Beta should have correct check value', () => {
+        cy.get("#componentInBetaCheckbox").should("be.checked");
+    });
+    it('PlayWithFint should have correct check value', () => {
+        cy.get("#componentPWFCheckbox").should("not.be.checked");
+    });
+    it('Production should include production link', () => {
+        cy.get("#componentProductionCell").contains("https://api.felleskomponent.no/test/kodeverk");
+    });
+    it('Beta should include production link', () => {
+        cy.get("#componentBetaCell").contains("https://beta.felleskomponent.no/test/kodeverk");
+    });
+    it('PlayWithFint should include production link', () => {
+        cy.get("#componentPWFCell").contains("https://play-with-fint.felleskomponent.no/test/kodeverk");
+    });
+    it('Swagger Production link should include production link', () => {
+        cy.get("#componentProductionSwaggerCell").contains("https://api.felleskomponent.no/test/kodeverk/swagger-ui.html");
+    });
+    it('Swagger Beta link should include production link', () => {
+        cy.get("#componentBetaSwaggerCell").contains("https://beta.felleskomponent.no/test/kodeverk/swagger-ui.html");
+    });
+    it('Swagger PWF link should include production link', () => {
+        cy.get("#componentPWFSwaggerCell").contains("https://play-with-fint.felleskomponent.no/test/kodeverk/swagger-ui.html");
+    });
+    it('Ok button should bring us back and remove the pop up', () => {
+        cy.get("#componentOKButton").should("be.visible");
+        cy.get("#componentOKButton").click();
+        cy.get("#form-dialog-title").should("not.exist");
+    });
+    */
+});
