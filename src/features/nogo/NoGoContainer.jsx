c3ef2bfaae8e9e54d0537336dc3d488841cc0265
Asgeir Nilsen <asgeir@fintlabs.no>
2021-03-22 10:15:13 +0000

    Capture host suffix from location hostname.

--- src/features/nogo/NoGoContainer.jsx
+++ src/features/nogo/NoGoContainer.jsx
@@ -7,15 +7,21 @@ class NoGoContainer extends Component {
   render() {
     const { contactExists, contactHasOrganisations } = this.props;
     let message, gotoUrl, buttonTitle;
+    const hostPattern = /^kunde(-[a-z]+)?\./;
+    let match = hostPattern.match(window.location.hostname);
+    let suffix = "";
+    if (match && match[1]) {
+      suffix = match[1];
+    }
     if (!contactExists) {
       message = "Du har ikke opprettet bruker.";
-      gotoUrl = "https://registrering.felleskomponent.no";
+      gotoUrl = `https://registrering${suffix}.felleskomponent.no`;
       buttonTitle = "Trykk her for å opprette konto";
     }
     if (contactExists && !contactHasOrganisations) {
       message =
         "Du er ikke tilknyttet en organisasjon. Gå til FINT administratoren i organisasjonen din for å få tilgang.";
-      gotoUrl = "https://kunde.felleskomponent.no";
+      gotoUrl = `https://kunde${suffix}.felleskomponent.no`;
       buttonTitle = "Trykk her for å gå til Kundeportalen";
     }
     return (
