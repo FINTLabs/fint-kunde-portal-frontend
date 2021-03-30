570bade226395cffdcbdbd56af8f89abb32adebe
Asgeir Storesund Nilsen <asgeir@fintlabs.no>
2021-03-30 09:12:44 +0200

    Revert unintentional commit.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,6 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(proxy('/api', { target: 'http://host.docker.internal:8080' }));
-    app.use(proxy('/tickets', { target: 'http://host.docker.internal:8080' }));
-    app.use(proxy('/unleash', { target: 'https://unleash-beta.fintlabs.no/api' }));
+    app.use(proxy('/api', { target: 'http://localhost:8080/' }));
+    app.use(proxy('/tickets', { target: 'http://localhost:8083/' }));
 };
