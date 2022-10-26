698a0070186e18ef7d06a30693bff6a0056eb158
Jennifer <97523482+jennifer-fintlabs@users.noreply.github.com>
2022-10-26 13:02:43 +0200

    Samtykke with toggle

--- src/main/appmenu/MenuItems.jsx
+++ src/main/appmenu/MenuItems.jsx
@@ -104,7 +104,7 @@ const MenuItems = () => {
                     </ListItem>
                 </RoleAuthorizationMenu>
             </FeatureToggle>
-            <FeatureToggle feature="samtykke-admin">
+            <FeatureToggle feature="∫samtykke-admin">
                 <RoleAuthorizationMenu role="ROLE_LOG">
                     <ListItem button component={Link} to="/samtykke">
                         <ListItemIcon>
