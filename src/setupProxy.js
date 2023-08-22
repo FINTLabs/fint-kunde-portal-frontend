7a6e64ff9a14223a535850489a3df3247b3e73e0
Jennifer <97523482+jennifer-fintlabs@users.noreply.github.com>
2023-08-22 10:20:31 +0200

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,5 +4,5 @@ module.exports = function(app) {
     app.use(proxy('/api', { target: 'http://localhost:8080/' }));
     app.use(proxy('/tickets', { target: 'http://localhost:8083/' }));
     app.use(proxy('/services', { target: 'http://localhost:8081/' }));
-    app.use(proxy('/consentadmin', { target: 'http://localhost:8081/' }));
+    app.use(proxy('/consentadmin', { target: 'https://kunde-beta.felleskomponent.no/' }));
 };
