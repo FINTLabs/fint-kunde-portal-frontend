4387d28ac67bb272b8d64c28667e4f9563433a50
Jennifer <97523482+jennifer-fintlabs@users.noreply.github.com>
2024-02-26 13:54:30 +0100

    added a debug line for testing 🐰 (#142)

--- src/data/redux/dispatchers/features.js
+++ src/data/redux/dispatchers/features.js
@@ -4,6 +4,7 @@ import {fetchFeaturesError, fetchFeaturesSuccess} from "../actions/features";
 export function fetchFeatures() {
     return (dispatch) => {
         return FeatureToggleApi.getFeatures().then((response) => {
+            console.log("FEATURES:", response);
             if (response.status === 200) {
                 dispatch(fetchFeaturesSuccess(response.data));
             } else {
