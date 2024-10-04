376f5ad2a4d741a61395e68d9f0975973bc646d8
jennifer-fintlabs <jennifer@fintlabs.no>
2024-10-04 15:40:34 +0200

    url me 🐰

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,13 +1,5 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    // app.use(proxy('/api', { target: 'http://localhost:8080/' }));
-//     app.use(proxy('/zendesk', { target: 'http://localhost/' })); // https://kunde-beta.felleskomponent.no/
-//     app.use(proxy('/services', { target: 'http://localhost:8081/' }));
-//     app.use(proxy('/consent-admin', { target: 'http://localhost:8084/' }));
-    app.use(proxy('/api', { target: 'http://fint-kunde-portal-backend:8080/'}));
+    app.use(proxy('/api', { target: 'http://fint-kunde-portal-backend:8080/', changeOrigin: true }));
 };
-// baseURL = 'http://localhost:8081/';
-baseURL = 'http://fint-kunde-portal-backend:8080/';
-
-// beta: https://kunde-beta.felleskomponent.no/
