6c61fc652d06fe9796f8d4fdaafc0a71feb40e11
jennifer-fintlabs <jennifer@fintlabs.no>
2024-10-04 16:13:34 +0200

    url me 🐰

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,13 @@
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(proxy('/api', {
-        target: 'http://fint-kunde-portal-backend:8080',
-        changeOrigin: true, // Ensures host header is rewritten to match target
-        secure: false,      // Disable SSL verification if needed
-        logLevel: 'debug'   // Adds detailed logging for debugging
-    }));
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: 'http://fint-kunde-portal-backend:8080',
+            changeOrigin: true,
+            secure: false,
+            logLevel: 'debug',
+        })
+    );
 };
