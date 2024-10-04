75f0a576a835324109c36e2843d6a22b945f816e
jennifer-fintlabs <jennifer@fintlabs.no>
2024-10-04 13:51:25 +0200

    me? 🐰

--- src/data/api/MeApi.js
+++ src/data/api/MeApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = 'https://kunde-old.felleskomponent.no';
+const API_URL = 'https://kunde.felleskomponent.no';
 class MeApi {
   static getMe() {
       return axios.get(`${API_URL}/api/me`);
