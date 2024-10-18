41e71c1b9693d663fc218462099a6794d640c397
jennifer-fintlabs <jennifer@fintlabs.no>
2024-10-18 13:49:21 +0200

    trying something else 🐰

--- src/data/api/MeApi.js
+++ src/data/api/MeApi.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 class MeApi {
   static getMe() {
+      const API_URL = process.env.BASE_PATH;
+      console.log("INSIDE ME CALL:", API_URL);
       return axios.get(`/api/me`);
   }
 }
