6a8b686b6c4b5012f2afa465b9db7bc6af75cefa
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2024-10-22 10:17:12 +0200

    turn back into old proxy setup

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,18 +1,7 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(createProxyMiddleware('/api', {
-        target: 'http://fint-kunde-portal-backend:8080',
-        changeOrigin: true
-    }));
-
-    app.use(createProxyMiddleware('/services', {
-        target: 'http://localhost:8081/',
-        changeOrigin: true
-    }));
-
-    app.use(createProxyMiddleware('/consent-admin', {
-        target: 'http://fint-samtykke-admin-backend:8080',
-        changeOrigin: true
-    }));
+    app.use(proxy('/api', {target: 'http://fint-kunde-portal-backend:8080'}));
 };
+
+//beta: https://kunde-beta.felleskomponent.no/
