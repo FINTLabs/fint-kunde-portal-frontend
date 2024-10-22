9fabe212e3c14ba2bed23532a324d6be67638229
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2024-10-22 10:40:02 +0200

    disable secure and cors

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(proxy('/api', {target: 'http://fint-kunde-portal-backend:8080'}));
+    app.use(proxy('/api', {target: 'http://fint-kunde-portal-backend:8080', secure: false, cors: false}));
 };
 
 //beta: https://kunde-beta.felleskomponent.no/
