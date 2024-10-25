4f0ba96e45eb0938c8c070be6bd76b8603c3a73d
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2024-10-25 09:46:18 +0200

    try putting url in api call

--- src/data/api/MeApi.js
+++ src/data/api/MeApi.js
@@ -3,7 +3,7 @@ class MeApi {
   static getMe() {
       const API_URL = process.env.BASE_PATH;
       console.log("INSIDE ME CALL:", API_URL);
-      return axios.get(`/api/me`);
+      return axios.get(`http://fint-kunde-portal-backend:8080/api/me`);
   }
 }
 
