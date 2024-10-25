19594708483cdbf5999beb7611590d21b44e9cc3
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2024-10-25 09:30:48 +0200

    change proxy setup

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,20 +1,15 @@
-const {createProxyMiddleware} = require("http-proxy-middleware");
-
+const { createProxyMiddleware } = require('http-proxy-middleware');
+console.log("using a proxy");
 module.exports = function(app) {
-    //app.use(proxy('/api', {target: 'http://fint-kunde-portal-backend:8080', secure: false, cors: false}));
+
     app.use(
         '/api',
         createProxyMiddleware({
             target: 'http://fint-kunde-portal-backend:8080',
-            secure: false,
             changeOrigin: true,
-            onProxyReq: (proxyReq, req, res) => {
-                console.log('Proxying request:', req.url);
-            },
-            onError: (err, req, res) => {
-                console.error('Error while proxying request:', err.message);
-                res.status(500).send('Proxy error');
-            }
+            secure: true,
+            logLevel: 'debug',
         })
     );
 };
+baseURL = 'http://fint-kunde-portal-backend:8080';
